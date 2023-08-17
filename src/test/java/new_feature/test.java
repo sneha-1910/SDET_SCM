0c5f3c962cd6befa6e6368d60a6cad5049c5c805
DELL <DELL@DESKTOP-VD4OOGI>
2023-08-17 12:15:54 +0530

    Project Framework

---
+++ src/test/java/new_feature/test.java
@@ -0,0 +1,16 @@
+package new_feature;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver	driver=new ChromeDriver();
+	}
+
+}
