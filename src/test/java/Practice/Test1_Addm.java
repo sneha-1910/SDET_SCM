acae90771fc65ae04b0aabae336a2b818cda7060
DELL <DELL@DESKTOP-VD4OOGI>
2023-08-17 11:36:33 +0530

    Frame

---
+++ src/test/java/Practice/Test1_Addm.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Test1_Addm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("manuf added");
+		
+	}
+
+}
